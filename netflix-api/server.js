3bb8d24839a01cc705ec33cc5327bf7b859db212
Dyana Rahhal <100005475+DyanaR@users.noreply.github.com>
2023-04-11 02:19:46 -0500

    Update server.js

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -3,8 +3,6 @@ const cors = require("cors");
 const userRoutes = require("./routes/UserRoutes");
 const mongoose = require("mongoose");
 
-require("dotenv").config()
-
 const PORT = process.env.PORT || 5000;
 
 const app = express();
